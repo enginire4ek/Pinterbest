c9e4377dd9ef7f5d86fad8de18a2a36f044902e9
enginire4ek <irekginiatov@yandex.ru>
2021-11-03 14:54:11 +0300

    Task 1(changes) - Add viewBinding in PinFeedHomeAdapter class

--- app/src/main/java/com/example/pinterbest/adapters/PinFeedHomeAdapter.kt
+++ app/src/main/java/com/example/pinterbest/adapters/PinFeedHomeAdapter.kt
@@ -1,21 +1,21 @@
 package com.example.pinterbest.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.pinterbest.R
 import com.example.pinterbest.data.PinObject
+import com.example.pinterbest.databinding.ViewHolderHomeFeedBinding
 
 class PinFeedHomeAdapter(private val pinObjects: List<PinObject>) :
     RecyclerView.Adapter<PinFeedHomeAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.view_holder_home_feed, parent, false)
-        return ViewHolder(view)
+        val binding = ViewHolderHomeFeedBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -26,13 +26,12 @@ class PinFeedHomeAdapter(private val pinObjects: List<PinObject>) :
         return pinObjects.size
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val pinImage: ImageView = itemView.findViewById(R.id.pin_image)
-        private val pinTitle: TextView = itemView.findViewById(R.id.pin_title)
+    class ViewHolder(private val binding: ViewHolderHomeFeedBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(pin: PinObject) {
-            pinImage.setImageResource(pin.imageLink)
-            pinTitle.text = pin.title
+            binding.pinImage.setImageResource(pin.imageLink)
+            binding.pinTitle.text = pin.title
         }
     }
 }

560af534b38db44a3b0d4713db3b94613b61573f
enginire4ek <irekginiatov@yandex.ru>
2021-12-03 14:49:47 +0300

    Task 4. Updates - UserSignUp.kt moved from data-layer to domain-layer

--- domain/src/main/java/com/example/pinterbest/domain/entities/UserSignUp.kt
+++ domain/src/main/java/com/example/pinterbest/domain/entities/UserSignUp.kt
@@ -1,6 +1,6 @@
-package com.example.pinterbest.data.models
+package com.example.pinterbest.domain.entities
 
-data class User(
+data class UserSignUp(
     val username: String,
     val email: String,
     val password: String

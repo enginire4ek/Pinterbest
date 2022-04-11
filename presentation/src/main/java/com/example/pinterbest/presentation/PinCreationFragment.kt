138c1aa66722e6628fef8b340b1156f0844617ce
enginire4ek <irekginiatov@yandex.ru>
2022-04-12 01:17:00 +0300

    - Updated retrofit2 version  - Added Swipe-to-Refresh  - Added README.md

--- presentation/src/main/java/com/example/pinterbest/presentation/PinCreationFragment.kt
+++ presentation/src/main/java/com/example/pinterbest/presentation/PinCreationFragment.kt
@@ -3,7 +3,6 @@ package com.example.pinterbest.presentation
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
